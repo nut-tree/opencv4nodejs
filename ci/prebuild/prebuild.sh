f15d10a736a18178c715d069a76ddbfce7707516
Simon Hofmann <mail@simon-hofmann.org>
2020-04-28 23:15:14 +0200

    Added explicit parameter for target version and runtime

--- ci/prebuild/prebuild.sh
+++ ci/prebuild/prebuild.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+
+TARGET=$1
+RUNTIME=$2
+
 cd ../../ &&\
 npm install --unsafe-perm &&\
 cd test
 npm install && npm test
 cd -
-npm run prebuild -- --include-regex "\.(node|a|so|dylib|lib|dll).*$" -u $GITHUB_TOKEN;
+npm run prebuild -- -t $TARGET -r $RUNTIME --include-regex "\.(node|a|so|dylib|lib|dll).*$" -u $GITHUB_TOKEN;
